9b6725284ed687a2dbf74377b01cf1ec1991e627
shraddha844 <shraddhadevadiga8123@gmail.com>
2026-02-22 17:39:39 +0530

    Add files via upload

---
+++ task1/fraud_detection.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, accuracy_score
+
+# Load dataset
+# Make sure creditcard.csv is in the same folder
+df = pd.read_csv("creditcard.csv")
+
+X = df.drop("Class", axis=1)
+y = df["Class"]
+
+# Scaling
+sc = StandardScaler()
+X = sc.fit_transform(X)
+
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Model
+model = LogisticRegression(max_iter=200)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print(classification_report(y_test, y_pred))
